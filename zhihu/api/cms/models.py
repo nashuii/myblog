e96beca177748ebb95e60a8cc0f353f088c789a4
nashuii <18825104432@163.com>
2017-11-18 00:10:23 +0800

    Remove the project 'zhihu'.

--- zhihu/api/cms/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class CmsUser(models.Model):
-    user = models.OneToOneField(User)
-    avatar = models.URLField(max_length=200,blank=True)
--- zhihu/api/common/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url,include
-from django.contrib import admin
-from api.common import views
-
-urlpatterns = [
-    url(r'^common_captcha/$', views.common_captcha,name='common_captcha'),
-
-]
